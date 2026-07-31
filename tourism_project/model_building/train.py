e56f27e04addf476c5272ec84577ffe59249345f
Atchuta Raviteja <41302889+RAVITeja2394@users.noreply.github.com>
2026-07-31 17:20:02 +0530

    Update project codebase

--- tourism_project/model_building/train.py
+++ tourism_project/model_building/train.py
@@ -92,7 +92,7 @@ else:
     print("⚠️ Ngrok URL missing. Logging locally inside repository context: ./mlruns")
     mlflow.set_tracking_uri("file:./mlruns")
 
-mlflow.set_experiment("Tourism_Package_XGBoost")
+mlflow.set_experiment("Tourism_Package_XGBoost_V1")
 
 # =====================================================================
 # 4. HYPERPARAMETER TUNING & MANUAL MLFLOW NESTED LOGGING
